63d9c768951d74565041c09b0f36965275fbc62a
liuyuefeixue.cool@163.com <liuyuefeixue.cool@163.com>
2016-04-22 10:32:50 +0800

    商品分类
    添加了一个无限极分类在org/util中

--- App/Admin/Controller/CateController.class.php
+++ App/Admin/Controller/CateController.class.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Admin\Controller;
 use Think\Controller;
 class CateController extends Controller {
@@ -6,25 +7,35 @@ class CateController extends Controller {
 	//分类列表
 		
     public function index(){
-    	//分类
+//		$Category = new \Org\Util\Category; 
+//		$cate=M("cate")->select();
+//		$m=$Category::unlimitedForLevel($cate,'&nbsp;&nbsp;--');
+//		p($m);die;
 		
-		$model = M('cate'); // 实例化User对象
-		$count = $model->count();// 查询满足要求的总记录数
-		$Page  = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(2)
 		
-		$Page->setConfig('header', '<li class="rows">共<b>%TOTAL_ROW%</b>条记录&nbsp;第<b>%NOW_PAGE%</b>页/共<b>%TOTAL_PAGE%</b>页</li>');
-        $Page->setConfig('prev', '上一页');
-        $Page->setConfig('next', '下一页');
-        $Page->setConfig('last', '末页');
-        $Page->setConfig('first', '首页');
-        $Page->setConfig('theme', '%FIRST%%UP_PAGE%%LINK_PAGE%%DOWN_PAGE%%END%%HEADER%');
-        $Page->lastSuffix = false;//最后一页不显示为总页数;
+		$cate=M("cate")->select();
+		$this->assign("data",$cate);
 		
-		$show  = $Page->show();// 分页显示输出// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-		$list  = $model->limit($Page->firstRow.','.$Page->listRows)->select();
-		$this->assign('data',$list);// 赋值数据集$this->assign('page',$show);// 赋值分页输出$this->display(); // 输出模板
-		$this->assign("show",$show);
-		$this->assign("count",$count);
+    	//分页
+//		$model = M('cate'); // 实例化User对象
+//		$count = $model->count();// 查询满足要求的总记录数
+//		$Page  = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(2)
+//		
+//		$Page->setConfig('header', '<li class="rows">共<b>%TOTAL_ROW%</b>条记录&nbsp;第<b>%NOW_PAGE%</b>页/共<b>%TOTAL_PAGE%</b>页</li>');
+//      $Page->setConfig('prev', '上一页');
+//      $Page->setConfig('next', '下一页');
+//      $Page->setConfig('last', '末页');
+//      $Page->setConfig('first', '首页');
+//      $Page->setConfig('theme', '%FIRST%%UP_PAGE%%LINK_PAGE%%DOWN_PAGE%%END%%HEADER%');
+//      $Page->lastSuffix = false;//最后一页不显示为总页数;
+//		
+//		$show  = $Page->show();// 分页显示输出// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+//		$list  = $model->limit($Page->firstRow.','.$Page->listRows)->select();
+//		$this->assign('data',$list);// 赋值数据集$this->assign('page',$show);// 赋值分页输出$this->display(); // 输出模板
+//		$this->assign("show",$show);
+//		$this->assign("count",$count);
+		
+		
 		$this->display();
 		
 	}
---
+++ App/Tool/Category.class.php
@@ -0,0 +1,97 @@
+﻿<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2009 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+namespace Org\Util;
+/**
+ * 日期时间操作类
+ * @category   ORG
+ * @package  ORG
+ * @subpackage  Category
+ * @author    liu21st <liu21st@gmail.com>
+ * @version   $Id: Date.class.php 2662 2012-01-26 06:32:50Z liu21st $
+ */
+Class Category{
+		//找子级，是不是自己的子级
+        //组合一维数组		
+		Static Public function unlimitedForLevel($cate,$html = '--',$pid =0,$level=0){
+			$arr=array();
+			foreach($cate as $v){
+				//如果是它的子级压倒数组中
+				//如果两个pid相等证明是，pid=0,是顶级
+				if($v['pid'] == $pid){					
+					$v['level']==$level + 1;
+					//将level级，替换成--符号
+					$v['html'] = str_repeat($html,$level);
+					//将数据放到新数组中
+					$arr[]=$v;
+					$arr =array_merge($arr,self::unlimitedForLevel($cate,$html,$v['pid'],$level+1));
+				}
+			}
+			return $arr;
+		}
+		
+		//组合多维数组
+		//首页导航菜单
+		Static Public function unlimitedForLayer($cate,$name='child',$pid=0){
+			$arr=array();
+			foreach($cate as  $v){
+				if($v['pid']==$pid){
+					$v['name']=self::unlimitedForLayer($cate,$name,$v['id']);
+				}
+				$arr[]=$v;
+			}
+			return $arr;
+		}
+		
+		//面包屑
+		Static Public function getParents($cate,$id){
+			$arr = array();
+			foreach($cate as $v){
+				if($v['id']=$id){
+					$arr[]=$v;
+					//颠倒合并顺序，进行数组反转
+					$arr=array_merge(self::getParents($cate,$v['pid']),$arr);
+				}
+			}
+			return $arr;
+		}
+		
+		//传递一个父级ID返回所有子分类id
+		Static Public function getChildsId($cate,$pid){
+			$arr = array();
+			foreach($cate as $v){
+				if($v['pid']=$pid){
+					$arr[]=$v['id'];
+					//颠倒合并顺序，进行数组反转
+					$arr=array_merge($arr,self::getChildsId($cate,$v['id']));
+				}
+			}
+			return $arr;
+		}
+		
+		//传递一个父级ID返回所有子分类的信息
+		Static Public function getChilds($cate,$pid){
+			$arr = array();
+			foreach($cate as $v){
+				if($v['pid']=$pid){
+					$arr[]=$v;
+					//颠倒合并顺序，进行数组反转
+					$arr=array_merge($arr,self::getChildsId($cate,$v['id']));
+				}
+			}
+			return $arr;
+		}
+
+		
+				
+				
+	}
+?>
